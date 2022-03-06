f99e675a7d27f0d20fbcbef936454a9df7a1659d
Juthamart.S <juthamart.sartta@gmail.com>
2022-03-06 22:39:44 +0700

    Added User Registration

---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/202203061444193_InitialCreate.cs
@@ -0,0 +1,69 @@
+namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BasketItems",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Basketid = c.String(maxLength: 128),
+                        ProductId = c.String(),
+                        Quanity = c.Int(nullable: false),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Baskets", t => t.Basketid)
+                .Index(t => t.Basketid);
+            
+            CreateTable(
+                "dbo.Baskets",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.ProductCategories",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Category = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(maxLength: 20),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Category = c.String(),
+                        Image = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.BasketItems", "Basketid", "dbo.Baskets");
+            DropIndex("dbo.BasketItems", new[] { "Basketid" });
+            DropTable("dbo.Products");
+            DropTable("dbo.ProductCategories");
+            DropTable("dbo.Baskets");
+            DropTable("dbo.BasketItems");
+        }
+    }
+}
