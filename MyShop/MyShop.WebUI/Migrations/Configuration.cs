eafce1899ea281abf322c46744c8a16725e28252
Juthamart.S <juthamart.sartta@gmail.com>
2022-03-06 21:51:40 +0700

    14.BasketUI

--- MyShop/MyShop.WebUI/Migrations/Configuration.cs
+++ MyShop/MyShop.WebUI/Migrations/Configuration.cs
@@ -1,18 +1,18 @@
-namespace MyShop.DataAccess.SQL.Migrations
+namespace MyShop.WebUI.Migrations
 {
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<MyShop.DataAccess.SQL.DataContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<MyShop.WebUI.Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(MyShop.DataAccess.SQL.DataContext context)
+        protected override void Seed(MyShop.WebUI.Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
